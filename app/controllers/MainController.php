83e241e695212e184e184a1900081ee50c753083
Apxat <apxata@gmail.com>
2018-12-19 08:35:46 +0300

    сделал роутер

---
+++ app/controllers/MainController.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Apxat
+ * Date: 19.12.2018
+ * Time: 8:29
+ */
+
+namespace app\controllers;
+
+
+class MainController
+{
+    public function indexAction(){
+        echo __METHOD__;
+    }
+}
---
+++ app/controllers/PageController.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Apxat
+ * Date: 19.12.2018
+ * Time: 8:32
+ */
+
+namespace app\controllers;
+
+
+class PageController
+{
+    public function viewAction(){
+        echo __METHOD__;
+    }
+}
